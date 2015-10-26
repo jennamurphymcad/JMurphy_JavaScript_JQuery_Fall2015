8ac095834f9b2793afbfc8f66c310840e1a4b38c
Jenna Murphy <jmurphy@mcad.edu>
2015-10-25 23:39:32 -0500

    updated main.js

--- Week_9/js/main.js
+++ Week_9/js/main.js
@@ -1,6 +1,9 @@
 /*Author: Jenna Murphy with assistance from jQuery API Documentation
 This script features a function to change text following an on click event of the mainHeading. Utilizing the jQuery API
 to implement jQuery's defined functionality.
+Slightly confused on whether $('#id') is equivalent to document.getElementById and which would be appropriate for this
+assignment. I have written the function for the jQuery $('#id') call and the javaScript getElementById. Both functions will respond
+the same.
 */
 
 $( document ).ready(function() {
@@ -15,3 +18,16 @@ $( document ).ready(function() {
     }
    })
 });
+
+// $( document ).ready(function() {
+//   var elementClicked = document.getElementById("mainHeading");
+//   elementClicked.onclick = function() {
+//   var response = prompt("What is your name?");
+//     if (response != null) {
+//      elementClicked.innerHTML = "Hello " + response + "!";
+//      elementClicked.css( "color", "#000" );
+//     } else {
+//      elementClicked.innerHTML = "Hello Stranger!";
+//     }
+//    }
+// });

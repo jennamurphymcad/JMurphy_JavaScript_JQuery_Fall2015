7936427d4a75d374cb81c38ed6ed974dbf6ce038
Jenna Murphy <jmurphy@mcad.edu>
2015-11-01 18:09:49 -0600

    set id attribute which fixed all issues!

--- Week_10/Exercise10.2/landonhotel/Start/main.js
+++ Week_10/Exercise10.2/landonhotel/Start/main.js
@@ -1,49 +1,20 @@
+/* Code written by Jenna Murphy with help from jQuery API - Specifically : http://api.jquery.com/attr/
+  updated gitHub and Assignment Files on 11/1/15
+*/
+
+var tableOfContents;
+
 $("document").ready(function() {
-  //clone all of the h2 elements
-  var tableOfContents = $("<ul id='tblOfCnts'>");
+  //create ul for Table of Contents
+  tableOfContents = $("<ul id='tblOfCnts'>");
+  //Insert ul after the h1
   tableOfContents.insertAfter("h1");
+  //set id attribute for each h2 heading
+  $("h2").attr( "id", function( i ) {
+    return "headingId" + i++;
+    });
+  //loop through each h2 heading and append to the ul
   $("h2").each(function() {
-  //  var index = $("h2").index(this);
-    $(tableOfContents).append("<li><a href='#" + this + "'>" + this + "</a>");
-
-  })
+    $(tableOfContents).append("<li><a href='#" + this.id + "'>" + $(this).text() + "</a>");
+  });
 });
-
-  //var h2Headings = $("h2");
-
-
-  // for (i = 0; i < h2Headings.length; i++) {
-  // //insert the cloned h2 elements after the "Specials" h1 element
-  //   var list = $("<li class='tableOfCntList'><a href='#" + this[i] +"'>" + this[i] +"</a>").insertAfter("h1");
-  // }
-  //   //wrap all <li> tags in a <ul>
-  // $(".tableOfCntList").wrapAll("<ul id='table'>");
-
-
-
-
-
-
-  // var h2Headings = $("h2");
-  // var tabelOfContents = $("<ul class='tableOfCnts'>");
-  // $(h2Headings).each(function(index, element) {
-  //   $(element).append("<li><a href= ")
-  //   // var current = $(this);
-  //   // current.attr("id", "title" + i);
-  //   // $("ol").append("<li><a id='link" +i + "' href='#title" +
-  //   // i + "' title ='" + current.attr('tagName') + "'>" + current.html() + "</a></li>");
-  //   // //$("<a href='#" + this + "></a>").append("h1");
-  // });
-
-
-//
-// $("document").ready(function() {
-//   $("ol").insertAfter("h1").addClass("tableOfContents");
-//   var h1Content = $("h2").clone();
-//   $("ol").add("<li><a href='#" + h1Content +"'</li>");
-//
-//   // $( "ul" ).add( "<p id='new'>new paragraph</p>" )
-//
-//   // h1Content.insertAfter("h1");
-//   // //$("<a href='#h1Content'>").appendTo("h1");
-// });

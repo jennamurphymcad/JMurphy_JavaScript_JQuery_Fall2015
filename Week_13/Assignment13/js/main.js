64d8c83441ea869c1304928bcbd8f02e11ef081d
Jenna Murphy <jmurphy@mcad.edu>
2015-11-23 22:41:30 -0600

    css update, read more/read less functionality

--- Week_13/Assignment13/js/main.js
+++ Week_13/Assignment13/js/main.js
@@ -1,31 +1,35 @@
 $(function() {
 
-  // $("#mainNav").navgoco({accordion: true});
+    //hide nested list items when javaScript is loaded
     $("#mainNav ul.recipe li").hide();
 
-    $("#mainNav h3").each(function(){
+    //add hidden class to div when javaScript is loaded
+    $("article div").addClass("hidden");
+
+    //reveal show more button when javaScript is loaded
+    $(".button").css("display", "block");
+
+    //toggle nested list items in each category of recipes
+    $("#mainNav h3").each(function() {
       $(this).click(function() {
         $("#" + this.id + "ul").children().slideToggle("slow");
       });
     });
 
-    $(".more").toggle(function(){
-    $(this).text("less..").siblings(".complete").show();
-  }, function(){
-    $(this).text("more..").siblings(".complete").hide();
-  });
+    //show more, show less toggle function
+    $("p.button").each(function() {
+      $(this).click(function() {
+        if ($("#" + this.id + "Div").hasClass("hidden")) {
+            $("#" + this.id + "Div").removeClass("hidden").addClass("show");
+            $(this).text("Show Less");
+        } else {
+            $("#" + this.id + "Div").addClass("hidden");
+            $(this).text("Read More...");
+        }
+      });
+    });
 
-    // $("#mainNav li.category").children().hide();
-    // $("#mainNav li.category").each(function() {
-    //   $(this).click(function(){
-    //     $(this).children().slideToggle("slow");
-    //   });
-    // });
-// this works now how to apply singularly
-    // $("#mainNav li.category").click(function() {
-    //     $("#mainNav ul.recipe li").slideToggle("slow");
-    //   });
-
+  //when clicked on recipe in nav, will only show the selected recipe and hide others
   $('#mainNav ul li ul li').click(function() {
     var selectedArticle = $("#" + this.id + "Content");
     //  $(selectedArticle).animate({"padding-top": "0px"}, {queue: false, duration: "slow"});
@@ -33,26 +37,18 @@ $(function() {
           if ($("#mainContent article").is(":visible")) {
             $("#mainContent article").hide();
             $(selectedArticle).fadeIn({duration: 1000});
+            $("article div").removeClass("hidden");
+            $(".button").css("display", "none");
           } else {
             $(selectedArticle).fadeOut({duration: 800});
           }
         } else {
           $("#mainContent article").hide();
           $(selectedArticle).fadeIn({duration: 1500});
-        //  $(selectedArticle).animate({"padding-top": "0px"}, {queue: false, duration: "slow"});
         }
     });
 
-  // $("#mainNav li")
-  //   .mouseover(function() {
-  //     $(this).addClass('listHover');
-  //   })
-  //   .mouseout(function() {
-  //     $(this).removeClass('listHover');
-  //   });
-
   $("#mainNav" ).click(function() {
     $( "h1" ).animate({left: "20px", "font-size": "1em"}, {queue: false, duration: "slow"});
-
-    });
+  });
 });

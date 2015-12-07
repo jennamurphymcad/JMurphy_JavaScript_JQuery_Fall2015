210fb761ce73e7e5a2d7838218f0b6e93f80eaf7
Jenna Murphy <jmurphy@mcad.edu>
2015-12-07 00:46:34 -0600

    creating full screen sketch, hover effect on screenshots

---
+++ Final/app.js
@@ -0,0 +1,42 @@
+var cell;
+var angle1=0;
+var angle2=0;
+var x1, y1;
+
+function setup() {
+ var myCanvas = createCanvas(windowWidth, windowHeight);
+ myCanvas.parent("myContainer");
+ myCanvas.position(0,0);
+ // frameRate(10);
+}
+function draw() {
+ background(255);
+ for (var i=0; i<=2500; i++) {
+    var cell = new Particle();
+    cell.display();
+    cell.move();
+  }
+}
+
+function Particle() {
+  this.x = random(windowWidth);
+  this.y = random(windowHeight);
+
+    this.move = function() {
+	  var ang1 = degrees(angle1);
+      var ang2 = degrees(angle2);
+	  this.x += width/2 + (5*cos(ang1));
+      this.y += height/2 + (15*sin(ang1));
+    };
+
+  this.display = function() {
+    strokeWeight(2);
+    stroke(235, 230, 210);
+    //fill(random(110, 153), 0, 0);
+  	ellipse(this.x, this.y, 15, 15);
+
+  angle1 += 2;
+  angle2 += 3;
+
+  };
+};
---
+++ Final/js/main.js
@@ -0,0 +1,67 @@
+$(function() {
+
+  //if JS available, hide figcaption p
+  $("figcaption p").addClass("hidden");
+
+  //if JS available, hide mainContent  $("#mainContent").addClass("hidden");
+
+  //click on h4 will reveal project description
+  $("figure").each(function() {
+    $(this).mouseenter(function() {
+        $("#" + this.id + "Par").removeClass("hidden").addClass("show");
+    });
+    $(this).mouseleave(function() {
+      $("#" + this.id + "Par").removeClass("show").addClass("hidden");
+    })
+  });
+
+  //clicking on landing page will allow mainContent to slideup
+  $("#myContainer").click(function() {
+    mainContentSlideUp();
+  });
+
+  // var canvas = $('canvas');
+  //
+  //     // Obtain a graphics context on the
+  //     // canvas element for drawing.
+  //     context = canvas.getContext('2d');
+  //
+  // // Start listening to resize events and
+  // // draw canvas.
+  // initialize();
+  //
+  // function initialize() {
+  //   // Register an event listener to
+  //   // call the resizeCanvas() function each time
+  //   // the window is resized.
+  //   window.addEventListener('resize', resizeCanvas, false);
+  //
+  //   // Draw canvas border for the first time.
+  //   resizeCanvas();
+  // }
+  //
+  // // Display custom canvas.
+  // // In this case it's a blue, 5 pixel border that
+  // // resizes along with the browser window.
+  // function redraw() {
+  //   context.strokeStyle = 'blue';
+  //   context.lineWidth = '5';
+  //   context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+  // }
+  //
+  // // Runs each time the DOM window resize event fires.
+  // // Resets the canvas dimensions to match window,
+  // // then draws the new borders accordingly.
+  // function resizeCanvas() {
+  //   canvas.width = window.innerWidth;
+  //   canvas.height = window.innerHeight;
+  //   redraw();
+  // }
+
+});
+
+function mainContentSlideUp() {
+  $("#mainContent").animate({
+  "margin-top": 0
+  }, "slow", "swing");
+};
